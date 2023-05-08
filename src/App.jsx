7962fef40a9bbf6955ef7e12c210c70172412ad7
jodosjodos <jeandedieu2030@gmail.com>
2023-05-08 15:08:17 +0200

    aside with tvshows

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState, useRef, useLayoutEffect, useMemo, useCallback } from 'react';
+import { useState, useRef, useMemo, useCallback, useEffect } from 'react';
 import Axios from 'axios'
 import { MovieCard } from './components/MovieCard';
 import  YouTube  from 'react-youtube';
@@ -12,88 +12,161 @@ function App() {
   const[selectedMovie,setSelectedMovie]=useState({})
   const [playTrailer,setPlayTrailer]=useState(false)
   const [notFound,setNotFound]=useState(false)
-  const apiUrl="https://api.themoviedb.org/3/";
+  const apiUrl="https://api.themoviedb.org/3";
+
   const apiKey="253eb6a5f22792e1412b47d42d247239"
+ 
   const fetchMovies = async (searchKey) => {
     const type = searchKey ? "search" : "discover";
     try {
-      const { data } = await Axios.get(`${apiUrl}/${type}/movie`, {
-        params: {
-          api_key: apiKey,
-          query: searchKey,
-        },
-      });
-      setMovies(data.results);
-      await selectMovie(data.results[0]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+      const movieResults = await Axios.get(`https://api.themoviedb.org/3/${type}/movie?api_key=${apiKey}`);
+      const tvShowsResults = await Axios.get(`https://api.themoviedb.org/3/${type}/tv?api_key=${apiKey}`);
+      if(movieResults.status===404 || tvShowsResults.status===404  ){
+        setNotFound(true)
+      }
   
-  const fetchMovie = async (id) => {
-    try {
-      const { data } = await Axios.get(`${apiUrl}/movie/${id}`, {
-        params: {
-          api_key: apiKey,
-          append_to_response: "videos",
-        },
-      });
-      return data;
+      const data = [...movieResults.data.results, ...tvShowsResults.data.results];
+  console.log(tvShowsResults.data.results);
+      setMovies(data);
+      await selectMovie(data[0]);
     } catch (error) {
       console.error(error);
     }
   };
-  const selectMovie=useCallback(async(movie)=>{
-    if(!movie){
+
+const fetchMoviesSearching = async (searchKey) => {
+  const type = searchKey ? "search" : "discover";
+  try {
+    const results = await Axios.get(`https://api.themoviedb.org/3/${type}/multi`, {
+      params: {
+        api_key: apiKey,
+        query: searchKey
+      }
+    });
+
+    if (results.data.results.length === 0) {
       setNotFound(true)
       return;
     }
-    try {
-      const data = await fetchMovie(movie.id);
-      // Now the movie object is loaded, so you can read its properties
-      setPlayTrailer(false);
-      setSelectedMovie(data);
-    } catch (error) {
-      console.log(error);
-      // handle error here, e.g. show an error message to the user
-      setPlayTrailer(false);
-      setSelectedMovie(movie);
+    if(results.status===404 || results.status===34){
+      setNotFound(true)
     }
-  },[movies])
-  
-  
 
-const renderTrailer = () => {
-  try {
-    if (selectedMovie && selectedMovie.videos && selectedMovie.videos.results && selectedMovie.videos.results.length > 0) {
-      const trailer = selectedMovie.videos.results.find((vid) => vid.name === "Official Trailer");
-      const key = trailer ? trailer.key : selectedMovie.videos.results[0].key;
-      return (
-        <YouTube
-          videoId={key}
-          className='youtubeContainer sm:w-20'
-         
-          opts={{
-            // width: "120px",
-            playerVars: {
-              autoplay: true,
-            },
-          }}
-        />
-      );
-    }
+    setMovies(results.data.results);
+    await selectMovie(results.data.results[0]);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
-  useLayoutEffect(()=>{
+  
+const fetchMovie = async (id) => {
+  try {
+    const response = await Axios.get(`${apiUrl}/movie/${id}`, {
+      params: {
+        api_key: apiKey,
+        append_to_response: "videos",
+      },
+    });
+
+    if (response.status === 200 && response.data) {
+      return response.data;
+    }
+
+    const tvShowResponse = await Axios.get(`${apiUrl}/tv/${id}`, {
+      params: {
+        api_key: apiKey,
+        append_to_response: "videos",
+      },
+    });
+
+    if (tvShowResponse.status === 200 && tvShowResponse.data) {
+      return tvShowResponse.data;
+    }
+
+    throw new Error("Movie or TV show not found");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const selectMovie = useCallback(async (movie) => {
+  if (!movie) {
+    setNotFound(true);
+    return;
+  }
+  try {
+    const data = await fetchMovie(movie.id);
+    setPlayTrailer(false);
+    if (data) {
+      if(data.videos <=0){
+        setNotFound(true)
+      }
+      
+      setSelectedMovie(data);
+    } else {
+      setNotFound(true);
+    }
+  } catch (error) {
+    console.log(error);
+    setPlayTrailer(false);
+    setSelectedMovie(movie);
+  }
+}, [movies]);
+
+  
+  
+
+  const renderTrailer = () => {
+    try {
+      if(selectedMovie.videos){
+        if (selectedMovie && selectedMovie.videos && selectedMovie.videos.results && selectedMovie.videos.results.length > 0) {
+          const trailer = selectedMovie.videos.results.find((vid) => vid.name === "Official Trailer");
+          const key = trailer ? trailer.key : selectedMovie.videos.results[0].key;
+          return (
+            <YouTube
+              videoId={key}
+              className=''
+             
+              opts={{
+                // width: "120px",
+                playerVars: {
+                  autoplay: true,
+                },
+              }}
+            />
+          );
+        } else {
+          console.log('no video');
+          setNotFound(true)
+          return
+         
+        
+        }
+      }else{
+console.log('no video');
+setNotFound(true) 
+return
+      }
+     
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+  useEffect(()=>{
       fetchMovies()
   },[])
   
   const renderMovies=()=>{
     
       return movies.map((movie)=>{
+        // if(!movie.videos){
+        //   console.log('reaching it');
+        //   setNotFound(true)
+        //   return
+        // }
           return <MovieCard
           key={movie.id}
           movie={movie}
@@ -106,25 +179,22 @@ const renderTrailer = () => {
   },[movies])
 // console.log(movies[0].original_title);
   const [searchKey,setSearchKey]=useState("")
+  
+
+
   const searching = (e) => {
-    setNotFound(false)
+    setNotFound(false);
     const targetValue = e.target.value;
-    if (targetValue.includes(" ")) {
-     
-      setSearchKey(targetValue);
-    } else {
-      setSearchKey(targetValue);
-    }
+    setSearchKey(targetValue);
+  };
    
 
 
-  };
   
-  const searchMovie=(e)=>{
-      e.preventDefault()
-      console.log(searchKey);
-      fetchMovies(searchKey)
-  }
+    const searchMovie = (e) => {
+      e.preventDefault();
+      fetchMoviesSearching(searchKey);
+    }
   
 
     const inputRef=useRef(null)
@@ -147,20 +217,26 @@ return(
      
     
       <form onSubmit={searchMovie} className="flex justify-center items-center">
-        <input type="text" name="searchInput" id="seachInput" onChange={searching} className="search" ref={inputRef} placeholder='search trailer....' />
-        <button type="submit" className=" searchButton" onClick={handleRef}><AiOutlineSearch size={40}/></button>
-      </form>
+  <input type="text" name="searchInput" id="seachInput" onChange={searching} className="search" ref={inputRef} placeholder='search trailer....' />
+  <button type="submit" className=" searchButton" onClick={handleRef}><AiOutlineSearch size={40}/></button>
+</form>
 
       
    </header>
    <section className={`h-[80vh] `}>
     
-     <div style={{background:`url(${imageUrl}${selectedMovie.backdrop_path}) `}} className='h-full cover'>
+   <div style={{background: selectedMovie.backdrop_path ? `url(${imageUrl}${selectedMovie.backdrop_path})` : `url(/img/noWallpaperFound.jpeg)` }} className='h-full cover'>
+
        <div className='all'>
+           {selectedMovie.videos ?
+           <>
+           
           {selectedMovie.videos && playTrailer ? (
           <>
            <div className='' >
-             { renderTrailer()}
+           
+
+
             </div>
             <div className='' >
                <h1 className='text-5xl'>{selectedMovie.title}</h1>
@@ -187,21 +263,25 @@ return(
 </div>
 
   )}
+  </>
+  :""}
        </div>
       
-      </div>
+      </div>  
    </section>
    <main>
-   <div className='grid grid-cols-3 gap-3 bg-black movie ' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+   <div className={`grid grid-cols-3 gap-3 bg-black movie ${notFound?"hidden":""}`} onClick={() => window.scrollTo({ top: 5, behavior: 'smooth' })}>
    {movies ?
           MemoMovies( )
       :
       
-      <div className='bg-blue-900'></div>}  
+      <div className='bg-blue-900'>movie not founds</div>}  
    </div>
    </main>
    <footer className='bg-black'>
-   {notFound && <p className='title'>no movie found with that name</p>}
+   {notFound && <p className='title'>no movie found with that name  or it has no trailer</p>}
+   
+   
    <p className='items-center flex justify-center'><FaCopyright color='#fff' size={30}/><span className='title'>jodos</span></p>
    </footer>
 
@@ -210,15 +290,3 @@ return(
 
 }
 export default App
-
-
-
-        
-         
-          
-
-
-   
-    
-  
-    
