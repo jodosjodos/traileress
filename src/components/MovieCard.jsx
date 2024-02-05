a44a3ba209d6388e00f5a1765d78bb226e872241
jodos <115969468+jodosjodos@users.noreply.github.com>
2024-02-05 09:12:29 +0200

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ export const MovieCard=({movie,selectMovie})=>{
   const moviUrl="https://image.tmdb.org/t/p/w500"
   
   return(
-    <div className="bg-red p-3 text-white text-xl rounded-lg other m-3" onClick={() => {selectMovie(movie)}}> 
+    <div className="bg-red p-3 text-white text-xl rounded-lg other m-3 hover:cursor-pointer" onClick={() => {selectMovie(movie)}}> 
     {movie.poster_path ? <img src={`${moviUrl}${movie.poster_path}`} alt="cover image" className="rounded-xl"/>
       :
     <div>no image found</div>
